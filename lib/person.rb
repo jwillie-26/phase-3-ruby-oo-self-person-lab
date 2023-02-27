0153b17e2623ec21056c322f72d0fa1b8a7c5353
jwillie-26 <105637783+jwillie-26@users.noreply.github.com>
2023-02-27 16:37:02 +0300

    fixed added solution

--- lib/person.rb
+++ lib/person.rb
@@ -1,83 +1,71 @@
 # your code goes here
-require "pry"
 class Person
-    attr_reader :name, :happiness, :hygiene
-    attr_accessor :bank_account
-    def initialize(name, bank_account = 25, happiness = 8, hygiene = 8)
+    attr_reader :name
+    attr_accessor :hygiene, :bank_account ,:happiness
+    def initialize(name)
         @name = name
-        @bank_account = bank_account
-        @happiness = happiness
-        @hygiene = hygiene
+        @bank_account = 25
+        @happiness = 8
+        @hygiene = 8
     end
 
-    def hygiene= hygiene
-        if hygiene >= 0 && hygiene <= 10
-            @hygiene = hygiene
-        elsif hygiene < 0
-            @hygiene = 0
-        elsif hygiene > 10
-            @hygiene = 10
-        else
-            hygiene
-        end
-    end
+     def initialize (name, bank_account=25, hygiene=8, happiness=8)
+    @name= name
+    @bank_account= bank_account
+    @happiness= happiness
+    @hygiene= hygiene
+  end  
 
-    def happiness= (happiness)
-        if happiness >= 0 && happiness <= 10
-            @happiness = happiness
-        elsif happiness < 0
-            @happiness = 0
-        elsif happiness > 10
-            @happiness = 10
-        else
-            happiness
-        end
-    end
+  def happiness=(value)
+    @happiness = value.clamp(0, 10)
+  end
+  
+  def hygiene=(value)
+    @hygiene = value.clamp(0, 10)
+  end 
+  
+  def happy?
+    self.happiness > 7
+  end
 
-    def clean?
-        self.hygiene > 7 ? true : false
-    end
+  def clean?
+    self.hygiene > 7
+  end
 
-    def happy?
-        self.happiness > 7 ? true : false
-    end
+  def get_paid (amount)
+    self.bank_account += amount
+    "all about the benjamins"
+  end  
 
-    def get_paid(amount)
-        self.bank_account += amount
-        "all about the benjamins"
-    end
+  def take_bath
+    self.hygiene += 4
+    "♪ Rub-a-dub just relaxing in the tub ♫"
+  end  
 
-    def take_bath
-        self.hygiene += 4
-        "♪ Rub-a-dub just relaxing in the tub ♫"
-    end
+  def work_out
+    self.hygiene -= 3
+    self.happiness += 2
+    "♪ another one bites the dust ♫"
+  end  
 
-    def work_out
-        self.happiness += 2
-        self.hygiene -= 3
-        "♪ another one bites the dust ♫"
-    end
+  def call_friend (friend)
+    friend.happiness += 3
+    self.happiness += 3
+    "Hi #{friend.name}! It's #{self.name}. How are you?"
+  end  
 
-    def call_friend(friend)
-        self.happiness += 3
-        friend_inst = Person.new(friend)
-        friend.happiness += 3
-        "Hi #{friend.name}! It's #{self.name}. How are you?"
-    end
+  def start_conversation (person, topic)
+    if topic == "politics"
+      person.happiness -= 2
+      self.happiness -= 2
+      "blah blah partisan blah lobbyist"
+    elsif topic == "weather"
+      self.happiness += 1
+      person.happiness += 1 
+      'blah blah sun blah rain' 
+    else 
+      'blah blah blah blah blah'  
+    end  
+  end  
 
-    def start_conversation(friend, topic)
-       if topic == "politics"
-        self.happiness -= 2
-        friend_inst = Person.new(friend)
-        friend.happiness -= 2
-        "blah blah partisan blah lobbyist"
-       elsif topic == "weather"
-        self.happiness += 1
-        friend_inst = Person.new(friend)
-        friend.happiness += 1
-        "blah blah sun blah rain"
-       else
-        "blah blah blah blah blah"
-       end
-    end
-end
+end  
